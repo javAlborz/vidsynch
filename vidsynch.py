0d524cf595bb7c009d36827dc7b033bfb9088372
javAlborz <alborz.sa@gmail.com>
2023-02-15 14:18:26 +0100

    final version

--- vidsynch.py
+++ vidsynch.py
@@ -1,7 +1,8 @@
 import cv2
+import numpy as np
 
 def synchronize_videos(video_list):
-    # Step 1: Reading the frames
+    # reading the frames
     frame_list = []
     for video in video_list:
         cap = cv2.VideoCapture(video)
@@ -14,33 +15,27 @@ def synchronize_videos(video_list):
                 break
         frame_list.append(frames)
 
-    # Step 2: Synchronizing the frames
+    # synchronizing the frames
     min_len = min([len(frames) for frames in frame_list])
     for i in range(len(frame_list)):
         frame_list[i] = frame_list[i][:min_len]
 
-    # Step 3: Writing the synchronized video
-    # Get the dimensions of the first video
-    cap = cv2.VideoCapture(video_list[0])
+    # writing the synchronized video
+    cap = cv2.VideoCapture(video_list[0]) # get dimension of first video
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    total_width = width * len(video_list)
+    out = cv2.VideoWriter('synchronized_video.avi',cv2.VideoWriter_fourcc(*'XVID'), 25.0, (total_width, height), True)
 
-    print(width)
-    print(height)
-
-    print((frame_list[0][0].shape[1], frame_list[0][0].shape[0]))
-
-    out = cv2.VideoWriter('synchronized_video.avi',cv2.VideoWriter_fourcc(*'XVID'), 25.0, (width, height), True) #needs final boolean?
-
-    for i in range(min_len):   
-        frame = []
-        for j in range(len(frame_list)):
-            frame.append(frame_list[j][i])
-        out.write(cv2.vconcat(frame))
+    for i in range(min_len):
+        concatenated_frame = np.concatenate(
+            [frame_list[j][i] for j in range(len(frame_list))],
+            axis=1
+        )
+        out.write(concatenated_frame)
     out.release()
 
-video_list = ['vid1.mp4', 'vid2.mp4', 'vid3.mp4']
-#video_list = ['vid1_converted.mp4', 'vid2_converted.mp4', 'vid3_converted.mp4']
+video_list = ['vid1.mp4', 'vid2.mp4']
 synchronize_videos(video_list)
 
-print("Synchronized video is saved'")
+print("Synchronized video is saved")
